93ced76c12c925ee5959e45f6b1fb38a64af25f9
JohannesSetiawan <88031010+JohannesSetiawan@users.noreply.github.com>
2022-09-12 16:53:47 +0700

    Update urls.py

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,7 +19,6 @@ from katalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
     path('', views.show_katalog, name='show_katalog'),
 ]
 
