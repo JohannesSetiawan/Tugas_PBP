d001a5ad85d8064fbe3ad840ddb9fea5387c4b6a
yohaneswijaya12 <yohaneswijaya12@gmail.com>
2022-09-12 16:39:35 +0700

    Mengerjakan tugas 2 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Johannes Setiawan'
+    }
+    return render(request, "katalog.html", context)
